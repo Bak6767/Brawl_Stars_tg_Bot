31f481e7dafbadc621ff2fe3b95fafe08904e6e3
Bak6767 <us030029@gmail.com>
2024-03-17 16:09:05 +0100

    переписал код для парсинга фото и имени игрока

--- Commands/parser_profile_photo_and_name.py
+++ Commands/parser_profile_photo_and_name.py
@@ -6,7 +6,22 @@ import os
 
 load_dotenv()
 
+#Функция для парсинга фотографии пользователя
 async def pars_profile_photo(profile_id):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
+    url = f"https://brawlbot.xyz/_next/image?url=https%3A%2F%2Fbrawlbot.xyz%2Fapi%2Fimage%2F{profile_id}&w=640&q=75"
+    req = requests.get(url, headers=headers)
+    
+    with open("profile_photo.jpg", "wb") as file:
+        file.write(req.content)
+    
+    with open("profile_photo.jpg", "rb") as file:
+        photo = file.read()
+
+#Функция для парсинга имени пользователя
+async def pars_profile_name(profile_id):
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
     }
@@ -22,7 +37,8 @@ async def pars_profile_photo(profile_id):
     soup = BeautifulSoup(src, "lxml")
     brawl_name = soup.find_all(class_="text-2xl font-bold mb-3 text-white")
     
+    result = None
+    
     for i in brawl_name:
         result = i.mark.text
-        print(result)
-    
+    return result
