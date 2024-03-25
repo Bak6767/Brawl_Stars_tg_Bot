99286147e5966ce62ae5e6a669fc8489825d4906
Bak6767 <us030029@gmail.com>
2024-03-25 10:09:01 +0100

    Код для кнопок был оптимизирован для улучшения читаемости кода и упрощения дальнейшей модификации.

--- battons/battons.py
+++ battons/battons.py
@@ -21,51 +21,29 @@ async def communication() -> InlineKeyboardMarkup:
 
 
 async def photo(user_id) -> InlineKeyboardMarkup:
-    profile_id_1 = str(await db.get_brawl_id_1(user_id))
-    profile_id_2 = str(await db.get_brawl_id_2(user_id))
-    profile_id_3 = str(await db.get_brawl_id_3(user_id))
-
     builder = InlineKeyboardBuilder()
-    if profile_id_1 == "False" or profile_id_1 == "None":
-        profile_id_1 = "+"
-        builder.row(InlineKeyboardButton(text = profile_id_1, callback_data = "add_brawl_id_1"))
-    else:
-        builder.row(InlineKeyboardButton(text = profile_id_1, callback_data = "brawl_id_1"))
-    if profile_id_2 == "False" or profile_id_2 == "None":
-        profile_id_2 = "+"
-        builder.row(InlineKeyboardButton(text = profile_id_2, callback_data = "add_brawl_id_2"))
-    else:
-        builder.row(InlineKeyboardButton(text = profile_id_2, callback_data = "brawl_id_2"))
-    if profile_id_3 == "False" or profile_id_3 == "None":
-        profile_id_3 = "+"
-        builder.row(InlineKeyboardButton(text = profile_id_3, callback_data = "add_brawl_id_3"))
-    else:
-        builder.row(InlineKeyboardButton(text = profile_id_3, callback_data = "brawl_id_3"))
-
+    for i in range(1, 4):
+        profile_id = str(await getattr(db, f'get_brawl_id_{i}')(user_id))
+        if profile_id in ["False", "None"]:
+            profile_id = "+"
+            callback_data = f"add_brawl_id_{i}"
+        else:
+            callback_data = f"brawl_id_{i}"
+        builder.row(InlineKeyboardButton(text=profile_id, callback_data=callback_data))
     return builder.as_markup()
+
 
 async def brawl_stars_name(user_id) -> InlineKeyboardMarkup:
-    profile_name_1 = str(await db.get_brawl_name_1(user_id))
-    profile_name_2 = str(await db.get_brawl_name_2(user_id))
-    profile_name_3 = str(await db.get_brawl_name_3(user_id))
-
     builder = InlineKeyboardBuilder()
-    if profile_name_1 == "False" or profile_name_1 == "None":
-        profile_name_1 = "+"
-        builder.row(InlineKeyboardButton(text = profile_name_1, callback_data = "add_brawl_id_1"))
-    else:
-        builder.row(InlineKeyboardButton(text = profile_name_1, callback_data = "brawl_id_1"))
-    if profile_name_2 == "False" or profile_name_2 == "None":
-        profile_name_2 = "+"
-        builder.row(InlineKeyboardButton(text = profile_name_2, callback_data = "add_brawl_id_2"))
-    else:
-        builder.row(InlineKeyboardButton(text = profile_name_2, callback_data = "brawl_id_2"))
-    if profile_name_3 == "False" or profile_name_3 == "None":
-        profile_name_3 = "+"
-        builder.row(InlineKeyboardButton(text = profile_name_3, callback_data = "add_brawl_id_3"))
-    else:
-        builder.row(InlineKeyboardButton(text = profile_name_3, callback_data = "brawl_id_3"))
-
+    for i in range(1, 4):
+        profile_name = str(await getattr(db, f'get_brawl_name_{i}')(user_id))
+        if profile_name in ["False", "None"]:
+            profile_name = "+"
+            callback_data = f"add_brawl_id_{i}"
+        else:
+            callback_data = f"brawl_id_{i}"
+        builder.row(InlineKeyboardButton(text=profile_name, callback_data=callback_data))
     return builder.as_markup()
+
 
 
