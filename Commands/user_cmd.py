c1f7a508baeee4cf0c1c6885c72224af4ebf9231
Bak6767 <us030029@gmail.com>
2024-03-25 21:16:58 +0100

    В этом обновлении был исправлен баг с залипанием кнопок

--- Commands/user_cmd.py
+++ Commands/user_cmd.py
@@ -1,9 +1,9 @@
-
-from aiogram.types import Message, InlineKeyboardMarkup, CallbackQuery, InlineKeyboardButton
 from Commands import parser_profile_photo_and_name as pars
-from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.state import StatesGroup, State
 from aiogram import types, Router, F, Bot
+from aiogram.fsm.context import FSMContext
 from aiogram import exceptions
 from dotenv import load_dotenv
 from database import bd as db
@@ -13,6 +13,11 @@ import os
 rp = Router()
 load_dotenv()
 bot = Bot(token = os.getenv("TOKEN_API"))
+
+class router_for_command_brawl_stars(StatesGroup):
+    brawl_id = State()
+    nume = State()
+    user_id = State()
 
 #Обработка команды /start
 cmd_list = "\n1. /start - Запустить/перезапустить бота\n2. /Brawl_Stars - Взаимодействие с твоим Brawl Stars id"
@@ -25,56 +30,28 @@ async def start(message: Message):
 async def key(message: types.Message):
     from battons.battons import communication
     markup = await communication()
-    await message.answer("Вибери действие:", reply_markup = markup)
+    await message.answer("Выбери действие:", reply_markup = markup)
 
 #Парсер фотографии профиля
-#Состояние ✅
 async def pars_profile_photo(message: Message, profile_id, user_id):
     try:
         await pars.pars_profile_photo(profile_id)
-        profile_photo = "D:\Project\Tg-Brawl-Bot\profile_photo.jpg"
+        profile_photo = "D:\Project\Tg-Brawl-Bot\profile_photo.webp"
         await message.answer_photo(photo = types.FSInputFile(path = profile_photo))
         if os.path.exists(profile_photo):
             os.remove(profile_photo)
     except exceptions.TelegramBadRequest:
         await change_brawl_id(message, user_id)
 
-#Функция которая изменяет вибраний Btawl Stars ID
-#Состояние ✅
+#Функция которая изменяет вибраной Brawl Stars ID
 async def change_brawl_id(message: Message, user_id):
-    await message.answer("Похоже вы указали не правиьный Brawl Stars ID.")
+    await message.answer("Похоже вы указали не правильный Brawl Stars ID.")
     await message.answer("Напиши еще раз свой Brawl Stars ID(без #):")
     @rp.message(F.text)
     async def profile_bs_id(message: Message):
         brawl_id = str(message.text.upper())
         await db.save_brawl_stars_id_1(user_id, brawl_id)
         await pars_profile_photo(message, brawl_id, user_id)
-
-
-#Функция которая проверяет есть лы такой user если да тогда возвращает нужний Brawl Stars ID
-#Состояние ✅
-async def send_profile_photo(message: Message, user_id, nume):
-    profile_id = str(await db.__getattribute__(f"get_brawl_id_{nume}")(user_id))
-    if profile_id == "False" or profile_id == "None":
-        await message.answer("Напиши свой Brawl Stars ID(без #):")
-        await db.save_user_id(user_id)
-        @rp.message(F.text)
-        async def profile_bs_id(message: Message):
-            brawl_id = str(message.text.upper())
-            await db.__getattribute__(f"save_brawl_stars_id_{nume}")(user_id, brawl_id)
-            await pars_profile_photo(message, brawl_id, user_id)
-            
-    else:
-        await pars_profile_photo(message, profile_id, user_id)
-
-
-#Функция которая сохраняет вибраний Brawl Stars ID
-async def save_brawl_stars_id(message: Message, user_id, nume):
-    await message.answer("Напиши свой Brawl Stars ID(без #):")
-    @rp.message(F.text)
-    async def profile_bs_id(message: Message):
-        brawl_id = str(message.text.upper())
-        await db.__getattribute__(f"save_brawl_stars_id_{nume}")(user_id, brawl_id)
 
 #-------------------------------------------------------------#
 #Обработчики кнопок
@@ -83,55 +60,42 @@ async def save_brawl_stars_id(message: Message, user_id, nume):
 @rp.callback_query(F.data == "photo")
 async def batton(call: CallbackQuery):
     await bot.delete_message(call.message.chat.id, call.message.message_id)
-    from battons.battons import photo
+    from battons.battons import brawl_stars_name
     user_id = call.from_user.id
-    await photo(user_id)
-    markup = await photo(call.from_user.id)
-    await call.message.answer("Виберете Brawl Stars ID из доступних вам:", reply_markup = markup)
+    markup = await brawl_stars_name(user_id)
+    await call.message.answer("Выберите Brawl Stars ID из доступных вам:", reply_markup = markup)
 
 @rp.callback_query(F.data == "change")
 async def batton(call: CallbackQuery):
     await bot.delete_message(call.message.chat.id, call.message.message_id)
     pass
 
-#Обработчики кнопоки add
-@rp.callback_query(F.data == "add_brawl_id_1")
-async def batton(call: CallbackQuery):
+#Обработчики кнопки add
+@rp.callback_query(F.data.contains("add_brawl_id_"))
+async def batton(call: CallbackQuery, state: FSMContext):
+    await state.set_state(router_for_command_brawl_stars.brawl_id)
     await bot.delete_message(call.message.chat.id, call.message.message_id)
     user_id = call.from_user.id
-    nume = 1
-    await send_profile_photo(call.message, user_id, nume)
-@rp.callback_query(F.data == "brawl_id_1")
-async def batton(call: CallbackQuery):
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    user_id = call.from_user.id
-    nume = 1
-    await send_profile_photo(call.message, user_id, nume)
+    nume = int(call.data.split("_")[-1])
+    await call.message.answer("Напиши свой Brawl Stars ID(без #):")
+    await db.save_user_id(user_id)
+    await state.update_data(nume = nume, user_id = user_id)
+@rp.message(router_for_command_brawl_stars.brawl_id)
+async def profile_bs_id(message: Message, state: FSMContext):
+    await state.update_data(brawl_id = message.text.upper())
+    data = await state.get_data()
+    nume = data["nume"]
+    user_id = data["user_id"]
+    brawl_name = await pars.pars_profile_name(data["brawl_id"])
+    await db.__getattribute__(f"save_brawl_stars_name_{nume}")(brawl_name, user_id)
+    await db.__getattribute__(f"save_brawl_stars_id_{nume}")(user_id, data["brawl_id"])
+    await pars_profile_photo(message, data["brawl_id"], user_id)
+    await state.clear()
 
-
-@rp.callback_query(F.data == "add_brawl_id_2")
+@rp.callback_query(F.data.contains("brawl_id_"))
 async def batton(call: CallbackQuery):
     await bot.delete_message(call.message.chat.id, call.message.message_id)
     user_id = call.from_user.id
-    nume = 2
-    await send_profile_photo(call.message, user_id, nume)
-@rp.callback_query(F.data == "brawl_id_2")
-async def batton(call: CallbackQuery):
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    user_id = call.from_user.id
-    nume = 2
-    await send_profile_photo(call.message, user_id, nume)
-
-
-@rp.callback_query(F.data == "add_brawl_id_3")
-async def batton(call: CallbackQuery):
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    user_id = call.from_user.id
-    nume = 3
-    await send_profile_photo(call.message, user_id, nume)
-@rp.callback_query(F.data == "brawl_id_3")
-async def batton(call: CallbackQuery):
-    await bot.delete_message(call.message.chat.id, call.message.message_id)
-    user_id = call.from_user.id
-    nume = 3
-    await send_profile_photo(call.message, user_id, nume)
+    nume = int(call.data.split("_")[-1])
+    brawl_id = str(await getattr(db, f'get_brawl_id_{nume}')(user_id))
+    await pars_profile_photo(call.message, brawl_id, user_id)
